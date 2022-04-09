294ba9f0fdeab5ba2009dc438320e82d811a7c5a
Damir Halilovic <dhalilov@dccsintra.net>
2022-04-09 21:01:34 +0200

    Added part 2 files

---
+++ workshop-part-1-and-2/CodeSnippets/maincomponent.js
@@ -0,0 +1,17 @@
+
+
+
+const MainView = () => {
+    // funkcjie i clanovi komponente
+    
+    
+    // sve sto funkcija renderuje
+    return (
+        <div>
+            <h1>Hello world from our first component</h1>
+        </div>
+    )
+    
+    }
+    
+    export default MainView
